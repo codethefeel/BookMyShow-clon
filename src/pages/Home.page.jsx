0e0d46651554bbb8330b1821761dc89cd72688b2
codethefeel <yogeshsolankim76@gmail.com>
2022-08-03 12:55:38 +0530

    home page api setup

--- src/pages/Home.page.jsx
+++ src/pages/Home.page.jsx
@@ -47,7 +47,6 @@ export default function HomePage() {
 
   return (
     <>
-    this is home page
       <div className="flex flex-col gap-16" >
         <div className="container mx-auto px-6 py-4">
           <h1 className="text-2xl font-bold text-gray-700 p-2">The Best of Entertainment</h1>
